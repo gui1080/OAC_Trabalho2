7575baed434068a5982a0d17abede35844ad343d
Victor Eduardo <43827624+Castorzaum@users.noreply.github.com>
2019-10-31 23:06:08 -0300

    Tudo funciona a principio

--- ULA/ula.vhd
+++ ULA/ula.vhd
@@ -2,6 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+
 entity ula is
 		generic (DATA_WIDTH : natural := 32);
 		port (
@@ -13,10 +14,9 @@ end entity ula;
 
 architecture ula of ula is
 
-signal   sinal_A_menor_com_sinal, sinal_A_menor_sem_sinal: STD_LOGIC_VECTOR (DATA_WIDTH -1 downto 0);
+signal   sinal_A_menor_com_sinal, sinal_default, sinal_A_menor_sem_sinal: STD_LOGIC_VECTOR (DATA_WIDTH -1 downto 0);
 signal   sinal_A_igual_B, sinal_A_dif_B: STD_LOGIC_VECTOR (DATA_WIDTH -1 downto 0);
 signal   sinal_A_maiorigual_B_com_sinal, sinal_A_maiorigual_B_sem_sinal: STD_LOGIC_VECTOR (DATA_WIDTH -1 downto 0);
-signal   B_vezes: STD_LOGIC;
 
 -- obs: "Algumas operações (ex.:+,-) não funcionarão com std_logic_vector.
 -- Para isso, será necessário converter para signed ou unsigned, realizar a
@@ -63,24 +63,22 @@ begin
 
 ---------------------------------------- process termina aqui
 
+sinal_default <= (others => '0');
 
-
-		sinal_default <= (others => '0');
-		B_vezes <= unsigned(B);
-
-		result <= std_logic_vector(signed(A) + signed(B))		when op="0000" else
-							std_logic_vector(signed(A) - signed(B))		when op="0001" else
-							shift_left(unsigned(A), B_vezes) when op="0010" else
-							sinal_A_menor_com_sinal when op="0011" else
-							sinal_A_menor_sem_sinal when op="0100" else
-							std_logic_vector(unsigned(A) xor unsigned(B)) when op="0101" else
-							shift_right(unsigned(A), B_vezes) when op="0110" else
-							shift_right(signed(A), B_vezes) when op="0110" else
-							std_logic_vector(unsigned(A) or unsigned(B)) when op="0101" else
-							std_logic_vector(unsigned(A) and unsigned(B)) when op="0101" else
-							sinal_A_igual_B when op="1010" else
-							sinal_A_dif_B when op="1011" else
-							sinal_A_maiorigual_B_com_sinal when op="1100" else
-							sinal_A_maiorigual_B_sem_sinal when op="1101" else
-							sinal_default;
+result <= std_logic_vector(signed(A) + signed(B))												when op="0000" else
+					std_logic_vector(signed(A) - signed(B))										when op="0001" else
+					std_logic_vector(shift_left(unsigned(A), to_integer(unsigned(B)))) 	when op="0010" else
+					sinal_A_menor_com_sinal 															when op="0011" else
+					sinal_A_menor_sem_sinal 															when op="0100" else
+					std_logic_vector(unsigned(A) xor unsigned(B)) 								when op="0101" else
+					std_logic_vector(shift_right(unsigned(A), to_integer(unsigned(B)))) 	when op="0110" else
+					std_logic_vector(shift_right(signed(A), to_integer(unsigned(B)))) 	when op="0110" else
+					std_logic_vector(unsigned(A) or unsigned(B)) 								when op="0101" else
+					std_logic_vector(unsigned(A) and unsigned(B)) 								when op="0101" else
+					sinal_A_igual_B 																		when op="1010" else
+					sinal_A_dif_B 																			when op="1011" else
+					sinal_A_maiorigual_B_com_sinal 													when op="1100" else
+					sinal_A_maiorigual_B_sem_sinal 													when op="1101" else
+					sinal_default;
 end ula;
+
