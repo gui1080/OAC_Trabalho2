fda872374f85678888a3ba56e36a4b3ae9de1640
Guilherme Braga <gbp1999@gmail.com>
2019-11-02 13:44:21 -0300

    com comentários

--- ULA/ula.vhd
+++ ULA/ula.vhd
@@ -1,18 +1,37 @@
+---------------------------------------------------------------------------------------------
+--		Organização e Arquitetura de Computadores - Turma C
+--			Trabalho 2 - VHDL
+--				Unb -2/2019
+--
+-- Nome: Gabriel Matheus			Matrícula: 17/0103498
+-- Nome: Guilherme Braga			Matrícula: 17/0162290
+-- Nome: Victor Eduardo			  Matrícula: 17/0115127
+--
+-- Link para o trabalho no Github --> https://github.com/therealguib545/OAC_Trabalho2
+--
+---------------------------------------------------------------------------------------------
+
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 
+-- Entradas e saídas da entidade:
 entity ula is
 		generic (DATA_WIDTH : natural := 32);
 		port (
-				A, B	: in std_logic_vector(DATA_WIDTH -1 downto 0);
-				op  	: in std_logic_vector(3 downto 0);
-				result: out std_logic_vector(DATA_WIDTH -1 downto 0)
+				A, B	: in std_logic_vector(DATA_WIDTH -1 downto 0);			-- Operando A e B
+				op  	: in std_logic_vector(3 downto 0);									-- Operação
+				result: out std_logic_vector(DATA_WIDTH -1 downto 0)			-- Resultado
 		);
 end entity ula;
 
 architecture ula of ula is
+
+-- Sinais de comparação
+-- Em um process, se faz todas as dadas comparações, e se atribui a resposta a um determinado vetor
+-- Ao final, apenas ligamos o resultado na saída caso seja pedido
 
 signal   sinal_A_menor_com_sinal, sinal_default, sinal_A_menor_sem_sinal: STD_LOGIC_VECTOR (DATA_WIDTH -1 downto 0);
 signal   sinal_A_igual_B, sinal_A_dif_B: STD_LOGIC_VECTOR (DATA_WIDTH -1 downto 0);
@@ -24,46 +43,50 @@ signal   sinal_A_maiorigual_B_com_sinal, sinal_A_maiorigual_B_sem_sinal: STD_LOG
 
 begin
 
-------------------------------------------------- process aqui
+------------------------------------------------- process começa aqui
 
 	process(op)
 		begin
 
-			sinal_A_menor_com_sinal <= (others => '0');
+			sinal_A_menor_com_sinal <= (others => '0');					-- enchemos o vetor com zeros
 			if (signed(A) < signed(B)) then
-					sinal_A_menor_com_sinal(0) <= '1';
+					sinal_A_menor_com_sinal(0) <= '1';							-- se a condição é satisfeita, colocamos o 1 na posição (0)
 			end if;
 
-			sinal_A_menor_sem_sinal <= (others => '0');
+			sinal_A_menor_sem_sinal <= (others => '0');					-- enchemos o vetor com zeros
 			if (unsigned(A) < unsigned(B)) then
-					sinal_A_menor_sem_sinal(0) <= '1';
+					sinal_A_menor_sem_sinal(0) <= '1';							-- se a condição é satisfeita, colocamos o 1 na posição (0)
 			end if;
 
-			sinal_A_igual_B <= (others => '0');
+			sinal_A_igual_B <= (others => '0');									-- enchemos o vetor com zeros
 			if (unsigned(A) = unsigned(B)) then
-					sinal_A_igual_B(0) <= '1';
+					sinal_A_igual_B(0) <= '1';											-- se a condição é satisfeita, colocamos o 1 na posição (0)
 			end if;
 
-			sinal_A_dif_B <= (others => '0');
+			sinal_A_dif_B <= (others => '0');										-- enchemos o vetor com zeros
 			if (unsigned(A) /= unsigned(B)) then
-					sinal_A_dif_B(0) <= '1';
+					sinal_A_dif_B(0) <= '1';												-- se a condição é satisfeita, colocamos o 1 na posição (0)
 			end if;
 
-			sinal_A_maiorigual_B_com_sinal <= (others => '0');
+			sinal_A_maiorigual_B_com_sinal <= (others => '0');	-- enchemos o vetor com zeros
 			if (signed(A) >= signed(B)) then
-					sinal_A_maiorigual_B_com_sinal(0) <= '1';
+					sinal_A_maiorigual_B_com_sinal(0) <= '1';				-- se a condição é satisfeita, colocamos o 1 na posição (0)
 			end if;
 
-			sinal_A_maiorigual_B_sem_sinal <= (others => '0');
+			sinal_A_maiorigual_B_sem_sinal <= (others => '0');	-- enchemos o vetor com zeros
 			if (unsigned(A) >= unsigned(B)) then
-					sinal_A_maiorigual_B_sem_sinal(0) <= '1';
+					sinal_A_maiorigual_B_sem_sinal(0) <= '1';				-- se a condição é satisfeita, colocamos o 1 na posição (0)
 			end if;
+
+			-- se a condição testada não é satisfeita, retornamos o vetor original cheio de zeros
 
 		end process;
 
 ---------------------------------------- process termina aqui
 
-sinal_default <= (others => '1');
+sinal_default <= (others => '1');													-- para debug: se a operação é inválida, retornamos um vetor apenas com 1 em todas as posições
+
+-- O switch case a seguir segue as especificações da ULA dadas no pedido do trabalho
 
 result <= std_logic_vector(signed(A) + signed(B))												when op="0000" else
 					std_logic_vector(signed(A) - signed(B))										when op="0001" else
@@ -81,4 +104,3 @@ result <= std_logic_vector(signed(A) + signed(B))												when op="0000" else
 					sinal_A_maiorigual_B_sem_sinal 													when op="1101" else
 					sinal_default;
 end ula;
-
--- ULA/ula_testbench.vhd
+++ ULA/ula_testbench.vhd
@@ -1,45 +1,27 @@
---------------------------------------------------------------------------------
--- Company: 
--- Engineer:
+---------------------------------------------------------------------------------------------
+--		Organização e Arquitetura de Computadores - Turma C
+--			Trabalho 2 - VHDL
+--				Unb -2/2019
 --
--- Create Date:   22:09:07 10/31/2019
--- Design Name:   
--- Module Name:   /home/castorzaum/Documents/Projetos/SD/Projetos ISE/ULA_OAC/ula-testbench.vhd
--- Project Name:  ULA_OAC
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: ula
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
+-- Nome: Gabriel Matheus			Matrícula: 17/0103498
+-- Nome: Guilherme Braga			Matrícula: 17/0162290
+-- Nome: Victor Eduardo			  Matrícula: 17/0115127
 --
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
---------------------------------------------------------------------------------
+-- Link para o trabalho no Github --> https://github.com/therealguib545/OAC_Trabalho2
+--
+---------------------------------------------------------------------------------------------
+
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 use ieee.numeric_std.all;
- 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---USE ieee.numeric_std.ALL;
- 
+
 ENTITY ula_testbench IS
 END ula_testbench;
- 
-ARCHITECTURE behavior OF ula_testbench IS 
- 
+
+ARCHITECTURE behavior OF ula_testbench IS
+
     -- Component Declaration for the Unit Under Test (UUT)
- 
+
     COMPONENT ula
     PORT(
          A : IN  std_logic_vector(31 downto 0);
@@ -48,7 +30,7 @@ ARCHITECTURE behavior OF ula_testbench IS
          result : OUT  std_logic_vector(31 downto 0)
         );
     END COMPONENT;
-    
+
 
    --Inputs
    signal A : std_logic_vector(31 downto 0) := (others => '0');
@@ -57,11 +39,7 @@ ARCHITECTURE behavior OF ula_testbench IS
 
  	--Outputs
    signal result : std_logic_vector(31 downto 0);
-   -- No clocks detected in port list. Replace <clock> below with 
-   -- appropriate port name 
- 
-   --constant <clock>_period : time := 10 ns;
- 
+
 begin
 
 	uut: ula PORT MAP (
@@ -73,130 +51,130 @@ begin
 
 	process
 	begin
-	
+
 	-- TESTE SUM OP 0000
-	
+
 	A <= ("00000000000000000000000000001010");
 	B <= ("00000000000000000000000000000101");
 	op <= "0000";
-	
+
 	wait for 20 ns;
-	
+
 	assert(result = "00000000000000000000000000001111") report "Falhou: op = 0000 - SUM" severity ERROR;
-	
+
 	-- TESTE SUB OP 0001
 
 	op <= "0001";
-	
+
 	wait for 20 ns;
-	
+
 	assert(result = "00000000000000000000000000000101") report "Falhou: op = 0001 - SUB" severity ERROR;
-	
+
 	-- TESTE SHIFT LEFT OP 0010
 
 	op <= "0010";
-	
+
 	wait for 20 ns;
-	
+
 	assert(result = "00000000000000000000000101000000") report "Falhou: op = 0010 - SHIFT LEFT" severity ERROR;
-	
+
 	-- TESTE A < B -SIGNED OP 0011
 
 	op <= "0011";
-	
+
 	wait for 20 ns;
-	
+
 	assert(result = "00000000000000000000000000000000") report "Falhou: op = 0011 - A < B -SIGNED" severity ERROR;
-	
+
 	-- TESTE A < B -UNSIGNED OP 0100
 
 	op <= "0100";
-	
+
 	wait for 20 ns;
-	
+
 	assert(result = "00000000000000000000000000000000") report "Falhou: op = 0100 - A < B -UNSIGNED" severity ERROR;
-	
+
 	-- TESTE A XOR B OP 0101
 
 	op <= "0101";
-	
+
 	wait for 20 ns;
-	
+
 	assert(result = "00000000000000000000000000001111") report "Falhou: op = 0101 - A XOR B" severity ERROR;
-	
+
 	-- TESTE A SHIFT RIGHT LOGIC B OP 0110
 
 	op <= "0110";
-	
+
 	wait for 20 ns;
-	
+
 	assert(result = "00000000000000000000000000000000") report "Falhou: op = 0110 - A SHIFT RIGHT LOGIC B" severity ERROR;
-	
+
 	-- TESTE A SHIFT RIGHT ARITH B OP 0111
 
 	op <= "0111";
-	
+
 	wait for 20 ns;
-	
+
 	assert(result = "00000000000000000000000000000000") report "Falhou: op = 0111 - A SHIFT RIGHT ARITH B" severity ERROR;
-	
+
 	-- TESTE A OR B OP 1000
 
 	op <= "1000";
-	
+
 	wait for 20 ns;
-	
+
 	assert(result = "00000000000000000000000000001111") report "Falhou: op = 1000 - A OR B" severity ERROR;
-	
+
 	-- TESTE A AND B OP 1001
 
 	op <= "1001";
-	
+
 	wait for 20 ns;
-	
+
 	assert(result = "00000000000000000000000000000000") report "Falhou: op = 1001 - A AND B" severity ERROR;
-	
+
 	-- TESTE A EQUAL B OP 1010
 
 	op <= "1010";
-	
+
 	wait for 20 ns;
-	
+
 	assert(result = "00000000000000000000000000000000") report "Falhou: op = 1010 - A EQUAL B" severity ERROR;
-	
+
 	-- TESTE A NOT EQUAL B OP 1011
 
 	op <= "1011";
-	
+
 	wait for 20 ns;
-	
+
 	assert(result = "00000000000000000000000000000001") report "Falhou: op = 1011 - A NOT EQUAL B" severity ERROR;
-	
+
 	-- TESTE A GREATER B (SIGNED) OP 1100
 
 	op <= "1100";
-	
+
 	wait for 20 ns;
-	
+
 	assert(result = "00000000000000000000000000000001") report "Falhou: op = 1100 - A GREATER B (SIGNED)" severity ERROR;
-	
+
 	-- TESTE A GREATER B (UNSIGNED) OP 1101
 
 	op <= "1101";
-	
+
 	wait for 20 ns;
-	
+
 	assert(result = "00000000000000000000000000000001") report "Falhou: op = 1101 - A GREATER B (UNSIGNED)" severity ERROR;
-	
+
 	-- TESTE DEFAULT OP 1111
 
 	op <= "1111";
-	
+
 	wait for 20 ns;
-	
+
 	assert(result = "11111111111111111111111111111111") report "Falhou: op = 1111 - DEFAULT" severity ERROR;
-	
+
 	report "FIM" severity NOTE;
-	
+
 	end process;
 end behavior;
