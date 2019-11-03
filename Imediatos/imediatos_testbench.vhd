c8d32a0cf883a24d4b776f567b5b73223a545a90
Guilherme Braga <gbp1999@gmail.com>
2019-11-02 22:39:14 -0300

    suposto fim do testbench

--- Imediatos/imediatos_testbench.vhd
+++ Imediatos/imediatos_testbench.vhd
@@ -43,11 +43,12 @@ BEGIN
           inst => inst,
           imm32 => imm32
         );
- 
+
    process
    begin
 
 	-- TESTE TIPO R
+  -- add t0, zero, zero
 
 	inst <= "00000000000000000000001010110011";
 
@@ -55,13 +56,56 @@ BEGIN
 
 	assert(imm32 = "00000000000000000000000000000000") report "Falhou: - tipo r" severity ERROR;
 
+
+  -- TESTE TIPO I
+  -- lw t0, 16(zero)
+
+  inst <= "00000001000000000010001010000011";
+
+	wait for 20 ns;
+
+	assert(imm32 = "00000000000000000000000000010000") report "Falhou: - tipo i" severity ERROR;
+
+
 	-- TESTE TIPO S
+  -- sw t0 60(s0)
 
 	inst <= "00000010010101000010111000100011";
 
 	wait for 20 ns;
 
 	assert(imm32 = "00000000000000000000000000111100") report "Falhou: - tipo s" severity ERROR;
+
+
+  -- TESTE TIPO B
+  -- bne t0, t0, main
+
+  inst <= "11111110010100101001000011100011";
+
+	wait for 20 ns;
+
+	assert(imm32 = "11111111111111111111111111110000") report "Falhou: - tipo b" severity ERROR;
+
+
+  -- TESTE TIPO U
+  -- lui s0, 2
+
+  inst <= "00000000000000000010010000110111";
+
+	wait for 20 ns;
+
+	assert(imm32 = "00000000000000000010000000000000") report "Falhou: - tipo u" severity ERROR;
+
+
+  -- TESTE TIPO J
+  -- jal rot
+
+  inst <= "00000000110000000000000011101111";
+
+	wait for 20 ns;
+
+	assert(imm32 = "00000000000000000000000000001100") report "Falhou: - tipo j" severity ERROR;
+
 
    end process;
 
